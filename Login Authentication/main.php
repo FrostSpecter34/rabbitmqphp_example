79f5de4ef63d448e81af5d32dd20eba0d1f42be5
FrostSpecter34 <drew.rod34@gmail.com>
2024-10-31 11:47:35 -0400

    Just for now

--- Login Authentication/main.php
+++ Login Authentication/main.php
@@ -15,45 +15,49 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="css/Homepage.css" />
+    <link rel="stylesheet" type="text/css" href="../Frontend Main/homepage.css" />
 </head>
-<body style="margin: 0; background: #ffffff">
-    <div class="container-center-horizontal">
-        <div class="frame-3 screen">
-            <div class="flex-col roboto-normal-black-64px">
-                <h1 class="sudo-squad-sub-manager">SUDO SQUAD SUB MANAGER</h1>
-                <div class="my-account">My Account</div>
-                <div class="active-subscriptions">Subscriptions</div>
+<body>
+    <header>
+        <h1 class="sudo-squad-sub-manager">SUDO SQUAD SUB MANAGER</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="addsub.php">Add subscription</a></li>
+            <li><a href="notifications.php">Notifications</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="my-account">My Account</div>
+        <div class="active-subscriptions">Subscriptions</div>
+        
+        <form method="GET" action="">
+            <label for="sort_by">Sort by:</label>
+            <select name="sort_by" id="sort_by" onchange="this.form.submit()">
+                <option value="name" <?php if ($sort_by == 'name') echo 'selected'; ?>>Name</option>
+                <option value="price" <?php if ($sort_by == 'price') echo 'selected'; ?>>Price</option>
+            </select>
+        </form>
 
-                <form method="GET" action="">
-                    <label for="sort_by">Sort by:</label>
-                    <select name="sort_by" id="sort_by" onchange="this.form.submit()">
-                        <option value="name" <?php if ($sort_by == 'name') echo 'selected'; ?>>Name</option>
-                        <option value="price" <?php if ($sort_by == 'price') echo 'selected'; ?>>Price</option>
-                    </select>
-                </form>
-
-                <div class="subscriptions-list">
-                    <?php foreach ($subscriptions as $subscription): ?>
-                        <div class="subscription-item">
-                            <div><?php echo $subscription['name']; ?> (<?php echo $subscription['status']; ?>)</div>
-                            <div class="subscription-details">
-                                <select>
-                                    <option>Price: <?php echo $subscription['price']; ?></option>
-                                    <option>Renewal Date: <?php echo $subscription['renewal_date']; ?></option>
-                                    <option>Cancellation Date: <?php echo $subscription['cancellation_date']; ?></option>
-                                </select>
-                            </div>
-                            <a href="cancel_subscription.php?id=<?php echo $subscription['id']; ?>">Cancel Subscription</a>
-                        </div>
-                    <?php endforeach; ?>
+        <div class="subscriptions-list">
+            <?php foreach ($subscriptions as $subscription): ?>
+                <div class="subscription-item">
+                    <div><?php echo $subscription['name']; ?> (<?php echo $subscription['status']; ?>)</div>
+                    <div class="subscription-details">
+                        <select>
+                            <option>Price: <?php echo $subscription['price']; ?></option>
+                            <option>Renewal Date: <?php echo $subscription['renewal_date']; ?></option>
+                            <option>Cancellation Date: <?php echo $subscription['cancellation_date']; ?></option>
+                        </select>
+                    </div>
+                    <a href="cancel_subscription.php?id=<?php echo $subscription['id']; ?>">Cancel Subscription</a>
                 </div>
-            </div>
-
-            <a href="addsub.php">Add subscription</a>
-            <a href="notifications.php">Notifications</a>
-            <a href="logout.php">Logout</a>
+            <?php endforeach; ?>
         </div>
-    </div>
+    </main>
+    <footer>
+        <p>&copy; 2024 SUDO SQUAD</p>
+    </footer>
 </body>
 </html>
