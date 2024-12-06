09044e148effc3ce56ca1d06c34d5752328a5e6b
Hami Chauhan <95922569+Hchauhan03@users.noreply.github.com>
2024-12-05 20:28:44 -0500

    Writing a function for deploy

--- Deployment-Server/deploy.php
+++ Deployment-Server/deploy.php
@@ -1,1 +1,14 @@
+<?php
 
+class Deployer
+{
+    private $DevServer;
+    private $QAServer;
+    private $ProdServer;
+
+      function __DeployTo()
+    {
+     $this->DevServer = new rabbitMQClient('DevServer');
+     $this->DevServer = new rabbitMQClient('QAServer');
+     $this->DevServer = new rabbitMQClient('ProdServer');
+    }
