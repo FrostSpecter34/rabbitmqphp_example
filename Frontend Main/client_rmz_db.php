85feed980f7bb962fe0768aac6753c0abdafe527
Hami Chauhan <95922569+Hchauhan03@users.noreply.github.com>
2024-12-09 20:44:13 -0500

    Update client_rmz_db.php

--- Frontend Main/client_rmz_db.php
+++ Frontend Main/client_rmz_db.php
@@ -5,10 +5,6 @@ require_once('../rabbitmq_files/get_host_info.inc');
 require_once('../rabbitmq_files/rabbitMQLib.inc');
 
 function createRabbitMQClientDatabase($request){
-
-	//Largely copied from the test client
-	//Use this to handle rmq requests to the database
-	//Still using testServer for now as it works but may change later
 	
 	$client = new rabbitMQClient("../rabbitmq_files/rabbitMQ_db.ini","testServer");
 
