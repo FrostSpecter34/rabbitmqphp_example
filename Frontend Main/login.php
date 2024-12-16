958573af99420c647036dcf370acaf5ad0895c13
Hami Chauhan <95922569+Hchauhan03@users.noreply.github.com>
2024-12-16 10:34:17 -0500

    Update login.php

--- Frontend Main/login.php
+++ Frontend Main/login.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['loggedin'] = true;
 
                 // Redirect to a protected page if login is successful
-                header('Location: http://www.sample.com/homepage.php'); 
+                header('Location: index.html'); 
                 exit;
             }
         } else {
