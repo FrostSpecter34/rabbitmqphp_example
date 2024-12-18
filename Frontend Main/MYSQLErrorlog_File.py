e845db428186969c7821b3bb5dc4bb70ecbad537
Hami Chauhan <95922569+Hchauhan03@users.noreply.github.com>
2024-12-18 14:57:13 -0500

    Added code in MYSQLErrorlog_File.py

--- Frontend Main/MYSQLErrorlog_File.py
+++ Frontend Main/MYSQLErrorlog_File.py
@@ -1,1 +1,47 @@
+requrire_once(testRabbitMQ.ini);
+import os
+import datetime
 
+<?php
+
+$connection = new AMQPStreamConnection('172.25.205.131', 5672, 'hami', 'hami', 'DMZ');
+$channel = $connection->channel();
+$channel->queue_declare('testQueue', false, true, false, false);
+$testExchange = 'testExchange';
+$fanout = 'fanout'; 
+$channel->exchange_declare($testExchange, $fanout, false, true, false);
+
+?>
+
+filepath = '/var/log/mysql/error.log'
+logSavedPath = '/etc/hami'
+
+if os.path.exists(filepath):
+    with open (filename) as file
+   logfile = file.read();
+    print(logfile);
+else:
+    return null;
+
+if (is_dir(logSavedpath))
+shutil.copy(filepath, logSavedpath);
+
+def logSaved(logName, logContent)
+return logSavedpath();
+
+timestamp = datetime();
+Content = [timestamp] logContent;
+
+with open('filepath', 'Content') as file:
+    file.write('logSavedpath')
+print(Content);
+
+<?php
+
+$msg = new AMQPMessage('Events by' Content);
+$channel->basic_publish($msg, $testExchange);
+
+
+$channel->close();
+$connection->close();
+?>
