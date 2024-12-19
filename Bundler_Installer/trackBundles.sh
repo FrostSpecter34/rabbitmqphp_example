73aa556360714f63d3eb60216c9f78b028f07405
Hami Chauhan <95922569+Hchauhan03@users.noreply.github.com>
2024-12-18 21:51:02 -0500

    Writing functions for trackBundles.sh

--- Bundler_Installer/trackBundles.sh
+++ Bundler_Installer/trackBundles.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+WATCH_DIR="/home/hami"
+
+script = "/home/hami/Documents/GitHub/rabbitmqphp_example/Bundler_Installer/Bundler_Installer/InsertIntoTable- Deployment.php"
+
+if [ ! -d "$WATCH_DIR" ]; then
+    echo "Directory $WATCH_DIR does not exist. Exiting."
+    exit 1
+fi
+
 
